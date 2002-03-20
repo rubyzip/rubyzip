c43bce5a18cbfd9dd92f31c6338a74b66117f930
thomas <thomas>
2002-03-20 20:18:35 +0000

    Fixed example.rb added example that shows zip file manipulation with Zip::ZipFile

--- example.rb
+++ example.rb
@@ -9,18 +9,17 @@ require 'zip'
 Zip::ZipInputStream.open("example.zip") {
   |zis|
   entry = zis.getNextEntry
-  puts "********* Zip entry '#{entry.name} (#{entry.size} bytes)' contains: ********"
-  puts zis.read
+  print "First line of '#{entry.name} (#{entry.size} bytes):  "
+  puts "'#{zis.gets.chomp}'"
   entry = zis.getNextEntry
-  puts "********* Zip entry '#{entry.name} (#{entry.size} bytes)' contains: ********"
-  puts zis.read
+  print "First line of '#{entry.name} (#{entry.size} bytes):  "
+  puts "'#{zis.gets.chomp}'"
 }
 
 
+####### Using ZipFile to read the directory of a zip file: #######
 
-####### Using BasicZipFile to read the directory of a zip file: #######
-
-zf = Zip::BasicZipFile.new("example.zip")
+zf = Zip::ZipFile.new("example.zip")
 zf.each_with_index {
   |entry, index|
   
@@ -29,6 +28,7 @@ zf.each_with_index {
   # entry can be the ZipEntry object or any object which has a to_s method that
   # returns the name of the entry.
 }
+
 
 ####### Using ZipOutputStream to write a zip file: #######
 
@@ -44,7 +44,22 @@ Zip::ZipOutputStream.open("exampleout.zip") {
   # the contents of exampleout.zip
 }
 
+####### Using ZipFile to change a zip file: #######
 
+Zip::ZipFile.open("exampleout.zip") {
+  |zf|
+  zf.add("thisFile.rb", "example.rb")
+  zf.rename("thisFile.rb", "ILikeThisName.rb")
+  zf.add("Again", "example.rb")
+}
+
+# Lets check
+Zip::ZipFile.open("exampleout.zip") {
+  |zf|
+  puts "Changed zip file contains: #{zf.entries.join(', ')}"
+  zf.remove("Again")
+  puts "Without 'Again': #{zf.entries.join(', ')}"
+}
 
 # For other examples, look at zip.rb and ziptest.rb
 
