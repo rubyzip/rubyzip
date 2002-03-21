6041b7377550b23c31106166871df465c5c0e888
thomas <thomas>
2002-03-21 21:12:19 +0000

    Added install.rb

---
+++ install.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+$VERBOSE = true
+
+require 'rbconfig'
+require 'find'
+require 'ftools'
+
+include Config
+
+files = %w{ zip.rb }
+
+files.each { 
+  |filename|
+  installPath = File.join(CONFIG["sitelibdir"], filename)
+  File::install(filename, installPath, 0644, true)
+}
