e16b5d1ea778e671902fcf112b4f9b36db9b2207
thomas <thomas>
2002-09-14 22:02:01 +0000

    *** empty log message ***

--- samples/zipfind.rb
+++ samples/zipfind.rb
@@ -9,22 +9,38 @@ require 'find'
 
 module ZipFind
   def self.find(path, zipfileRegex, fileNameRegex, breakOnMatch = true)
+    startedAt = Time.now
+    archivesExamined = 0
+    entriesExamined = 0
+    entriesFound = 0
     Find.find(path) {
       |file|
+      archivesExamined = archivesExamined.next
       if (file =~ zipfileRegex && File.readable?(file))
 	Zip::ZipFile.foreach(file) {
 	  |entry|
+	  entriesExamined = entriesExamined.next
 	  if entry.to_s =~ fileNameRegex
 	    reportEntryFound(file, entry)
+	    entriesFound = entriesFound.next
 	    return if breakOnMatch
 	  end
 	} rescue Errno::EACCES
       end
     }
+  ensure
+    reportStats(startedAt, archivesExamined, entriesExamined, entriesFound)
   end
   
   def self.reportEntryFound(zipfileName, entry)
     puts "Found entry #{entry} in zip file #{zipfileName}"
+  end
+
+  def self.reportStats(startedAt, archivesExamined, 
+		       entriesExamined, entriesFound)
+    seconds = (Time.now - startedAt).round
+    puts ("Found #{entriesFound} entries after examining #{entriesExamined} " +
+	  "entries in #{archivesExamined} archives in #{seconds} seconds.")
   end
   
   def self.usage
