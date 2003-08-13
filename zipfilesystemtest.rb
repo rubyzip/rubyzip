45e47fd0e842df9aa1cdaae8c2521e3c1654624d
thomas <thomas>
2003-08-13 08:02:32 +0000

    Updated and added missing copyright notices

--- zipfilesystemtest.rb
+++ zipfilesystemtest.rb
@@ -472,6 +472,6 @@ END {
   end
 }
 
-# Copyright (C) 2002 Thomas Sondergaard
+# Copyright (C) 2002, 2003 Thomas Sondergaard
 # rubyzip is free software; you can redistribute it and/or
 # modify it under the terms of the ruby license.
