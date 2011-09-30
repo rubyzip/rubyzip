f7406d359d7ae90f8d2008fa8e0b3ea3a1b275d9
Xavier Shay <xavier@rhnh.net>
2011-09-30 10:50:01 -0500

    Use RbConfig instead of deprecated Config.

--- lib/zip/compressor.rb
+++ lib/zip/compressor.rb
@@ -4,7 +4,7 @@ module Zip
 
   RUBY_MINOR_VERSION = RUBY_VERSION.split(".")[1].to_i
 
-  RUNNING_ON_WINDOWS = Config::CONFIG['host_os'] =~ /^win|mswin/i 
+  RUNNING_ON_WINDOWS = RbConfig::CONFIG['host_os'] =~ /^win|mswin/i
 
   # Ruby 1.7.x compatibility
   # In ruby 1.6.x and 1.8.0 reading from an empty stream returns 
