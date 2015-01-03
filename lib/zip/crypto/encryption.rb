23f2ead7ee2b999a7fd8af75eaf7be144f82706d
Johnny Shields <johnny.shields@gmail.com>
2015-01-04 03:36:53 +0900

    - Move encryption related files to /lib/zip/crypto/ dir - NullEncrypter and NullDecrypter should inherit Encrypter and Decrypter respectively - Add copyright notice to bottom of added files for consistency

--- lib/zip/crypto/encryption.rb
+++ lib/zip/crypto/encryption.rb
@@ -19,3 +19,7 @@ module Zip
     end
   end
 end
+
+# Copyright (C) 2002, 2003 Thomas Sondergaard
+# rubyzip is free software; you can redistribute it and/or
+# modify it under the terms of the ruby license.
--- lib/zip/crypto/traditional_encryption.rb
+++ lib/zip/crypto/traditional_encryption.rb
@@ -88,3 +88,7 @@ module Zip
     end
   end
 end
+
+# Copyright (C) 2002, 2003 Thomas Sondergaard
+# rubyzip is free software; you can redistribute it and/or
+# modify it under the terms of the ruby license.
