987dbf106431b8b0f566ac06404c74112cbdd53e
Pavel.Lobashov <dr-evil-lob@mail.ru>
2015-03-22 19:58:07 +0300

    fix rubocop Style/SpaceInsideBrackets cop

--- samples/gtkRubyzip.rb
+++ samples/gtkRubyzip.rb
@@ -70,9 +70,9 @@ class MainApp < Gtk::Window
     @zipfile = Zip::File.open(filename)
     @clist.clear
     @zipfile.each do |entry|
-      @clist.append([ entry.name,
-                      entry.size.to_s,
-                      (100.0*entry.compressedSize/entry.size).to_s+'%' ])
+      @clist.append([entry.name,
+                     entry.size.to_s,
+                     (100.0*entry.compressedSize/entry.size).to_s+'%'])
     end
   end
 end
