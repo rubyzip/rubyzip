366a1d8dca66b5a6fd344babd24f8a0a10ca329b
David Rodríguez <deivid.rodriguez@gmail.com>
2016-10-08 19:20:45 -0300

    Fix test failure when running a single test file
    ```
    bundle exec rake TEST=test/file_test.rb
    ```
    
    was giving
    
    ```
      1) Error:
    ZipFileTest#test_get_output_stream:
    NameError: uninitialized constant ZipFileTest::ZipEntryTest
        .../test/file_test.rb:59:in `block in test_get_output_stream'
        .../lib/zip/file.rb:102:in `open'
        .../test/file_test.rb:45:in `test_get_output_stream'
    ```

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -219,3 +219,16 @@ module ExtraAssertions
     anObject.instance_eval "undef #{method}; alias #{method} #{method}_org"
   end
 end
+
+module ZipEntryData
+  TEST_ZIPFILE = 'someZipFile.zip'
+  TEST_COMMENT = 'a comment'
+  TEST_COMPRESSED_SIZE = 1234
+  TEST_CRC = 325_324
+  TEST_EXTRA = 'Some data here'
+  TEST_COMPRESSIONMETHOD = ::Zip::Entry::DEFLATED
+  TEST_NAME = 'entry name'
+  TEST_SIZE = 8432
+  TEST_ISDIRECTORY = false
+  TEST_TIME = Time.now
+end
