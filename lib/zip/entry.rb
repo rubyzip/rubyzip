5e9c6851d3cbdee52a26ec0fd3c7a2882e1551c3
Marc Falla <mfalla@demarque.com>
2017-11-21 11:47:06 -0500

    Allow a Zip::File to return the file to avoid a File.open on each entries

--- lib/zip/entry.rb
+++ lib/zip/entry.rb
@@ -490,7 +490,7 @@ module Zip
 
     # Returns an IO like object for the given ZipEntry.
     # Warning: may behave weird with symlinks.
-    def get_input_stream(&block)
+    def get_input_stream(archive = nil, &block)
       if @ftype == :directory
         yield ::Zip::NullInputStream if block_given?
         ::Zip::NullInputStream
@@ -508,7 +508,7 @@ module Zip
         end
       else
         cd_entry = self
-        zis = ::Zip::InputStream.new(@zipfile, cd_entry, local_header_offset)
+        zis = ::Zip::InputStream.new(archive || @zipfile, cd_entry, local_header_offset)
         zis.instance_variable_set(:@internal, true)
         zis.get_next_entry
         if block_given?
