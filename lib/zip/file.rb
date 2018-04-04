03633933ebb714cdff1e3f5604434f35bad0e0cf
Robert Haines <rhaines@manchester.ac.uk>
2018-04-04 14:31:34 +0100

    No need to require stringio in Zip::File.open_buffer.
    It's already required in zip.rb.

--- lib/zip/file.rb
+++ lib/zip/file.rb
@@ -120,7 +120,6 @@ module Zip
           raise "Zip::File.open_buffer expects a String or IO-like argument (responds to #{IO_METHODS.join(', ')}). Found: #{io.class}"
         end
         if io.is_a?(::String)
-          require 'stringio'
           io = ::StringIO.new(io)
         elsif io.respond_to?(:binmode)
           # https://github.com/rubyzip/rubyzip/issues/119
