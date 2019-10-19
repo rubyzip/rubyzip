f58e38012cf4d7493f70ffdc4e9a583d94f72ad5
Robert Haines <rhaines@manchester.ac.uk>
2019-10-19 07:04:04 +0100

    Add the `options` parameter to `Zip::File.open`.

--- lib/zip/file.rb
+++ lib/zip/file.rb
@@ -107,8 +107,8 @@ module Zip
       # Same as #new. If a block is passed the ZipFile object is passed
       # to the block and is automatically closed afterwards just as with
       # ruby's builtin File.open method.
-      def open(file_name, create = false)
-        zf = ::Zip::File.new(file_name, create)
+      def open(file_name, create = false, options = {})
+        zf = ::Zip::File.new(file_name, create, false, options)
         return zf unless block_given?
         begin
           yield zf
