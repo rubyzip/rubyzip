4ac83737b146560bc02c2880a6565f691458832a
Jan-Joost Spanjers <oss@hiberis.nl>
2019-12-23 20:57:47 +0100

    Cleanup Inflater

--- lib/zip/inflater.rb
+++ lib/zip/inflater.rb
@@ -1,32 +1,33 @@
 module Zip
   class Inflater < Decompressor #:nodoc:all
-    def initialize(input_stream)
-      super(input_stream)
-      @zlib_inflater           = ::Zlib::Inflate.new(-Zlib::MAX_WBITS)
-      @output_buffer           = ''.dup
+    def initialize(*args)
+      super
+
+      @buffer = ''.dup
+      @zlib_inflater = ::Zlib::Inflate.new(-Zlib::MAX_WBITS)
       @has_returned_empty_string = false
     end
 
-    def sysread(number_of_bytes = nil, buf = '')
-      readEverything = number_of_bytes.nil?
-      while readEverything || @output_buffer.bytesize < number_of_bytes
-        break if internal_input_finished?
-        @output_buffer << internal_produce_input(buf)
+    def sysread(length = nil, buf = '')
+      while length.nil? || (@buffer.bytesize < length)
+        break if input_finished?
+        @buffer << produce_input(buf)
       end
-      return value_when_finished if @output_buffer.bytesize == 0 && eof?
-      end_index = number_of_bytes.nil? ? @output_buffer.bytesize : number_of_bytes
-      @output_buffer.slice!(0...end_index)
+
+      return value_when_finished if eof?
+
+      @buffer.slice!(0...(length || @buffer.bytesize))
     end
 
     def eof
-      @output_buffer.empty? && internal_input_finished?
+      @buffer.empty? && input_finished?
     end
 
     alias_method :eof?, :eof
 
     private
 
-    def internal_produce_input(buf = '')
+    def produce_input(buf = '')
       retried = 0
       begin
         @zlib_inflater.inflate(@input_stream.read(Decompressor::CHUNK_SIZE, buf))
@@ -39,7 +40,7 @@ module Zip
       raise(::Zip::DecompressionError, 'zlib error while inflating')
     end
 
-    def internal_input_finished?
+    def input_finished?
       @zlib_inflater.finished?
     end
 
