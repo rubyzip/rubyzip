7b2e9c7970de6f34f23cc282766cfda734865eed
Robert Haines <rhaines@manchester.ac.uk>
2021-06-02 21:37:20 +0100

    Extract `FileSystem::File::Stat` from `FileSystem::File`.

--- lib/zip/filesystem/file.rb
+++ lib/zip/filesystem/file.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'file_stat'
+
 module Zip
   module FileSystem
     # Instances of this class are normally accessed via the accessor
@@ -10,108 +12,6 @@ module Zip
     # similarity with the methods in File
     class File # :nodoc:all
       attr_writer :dir
-
-      class ZipFsStat
-        class << self
-          def delegate_to_fs_file(*methods)
-            methods.each do |method|
-              class_eval <<-END_EVAL, __FILE__, __LINE__ + 1
-                def #{method}                      # def file?
-                  @zip_fs_file.#{method}(@entry_name) #   @zip_fs_file.file?(@entry_name)
-                end                                # end
-              END_EVAL
-            end
-          end
-        end
-
-        def initialize(zip_fs_file, entry_name)
-          @zip_fs_file = zip_fs_file
-          @entry_name = entry_name
-        end
-
-        def kind_of?(type)
-          super || type == ::File::Stat
-        end
-
-        delegate_to_fs_file :file?, :directory?, :pipe?, :chardev?, :symlink?,
-                            :socket?, :blockdev?, :readable?, :readable_real?, :writable?, :ctime,
-                            :writable_real?, :executable?, :executable_real?, :sticky?, :owned?,
-                            :grpowned?, :setuid?, :setgid?, :zero?, :size, :size?, :mtime, :atime
-
-        def blocks
-          nil
-        end
-
-        def get_entry
-          @zip_fs_file.__send__(:get_entry, @entry_name)
-        end
-        private :get_entry
-
-        def gid
-          e = get_entry
-          if e.extra.member? 'IUnix'
-            e.extra['IUnix'].gid || 0
-          else
-            0
-          end
-        end
-
-        def uid
-          e = get_entry
-          if e.extra.member? 'IUnix'
-            e.extra['IUnix'].uid || 0
-          else
-            0
-          end
-        end
-
-        def ino
-          0
-        end
-
-        def dev
-          0
-        end
-
-        def rdev
-          0
-        end
-
-        def rdev_major
-          0
-        end
-
-        def rdev_minor
-          0
-        end
-
-        def ftype
-          if file?
-            'file'
-          elsif directory?
-            'directory'
-          else
-            raise StandardError, 'Unknown file type'
-          end
-        end
-
-        def nlink
-          1
-        end
-
-        def blksize
-          nil
-        end
-
-        def mode
-          e = get_entry
-          if e.fstype == 3
-            e.external_file_attributes >> 16
-          else
-            33_206 # 33206 is equivalent to -rw-rw-rw-
-          end
-        end
-      end
 
       def initialize(mapped_zip)
         @mapped_zip = mapped_zip
@@ -329,7 +229,7 @@ module Zip
       def stat(filename)
         raise Errno::ENOENT, filename unless exists?(filename)
 
-        ZipFsStat.new(self, filename)
+        Stat.new(self, filename)
       end
 
       alias lstat stat
---
+++ lib/zip/filesystem/file_stat.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+
+module Zip
+  module FileSystem
+    class File # :nodoc:all
+      class Stat # :nodoc:all
+        class << self
+          def delegate_to_fs_file(*methods)
+            methods.each do |method|
+              class_eval <<-END_EVAL, __FILE__, __LINE__ + 1
+                def #{method}                      # def file?
+                  @zip_fs_file.#{method}(@entry_name) #   @zip_fs_file.file?(@entry_name)
+                end                                # end
+              END_EVAL
+            end
+          end
+        end
+
+        def initialize(zip_fs_file, entry_name)
+          @zip_fs_file = zip_fs_file
+          @entry_name = entry_name
+        end
+
+        def kind_of?(type)
+          super || type == ::File::Stat
+        end
+
+        delegate_to_fs_file :file?, :directory?, :pipe?, :chardev?, :symlink?,
+                            :socket?, :blockdev?, :readable?, :readable_real?, :writable?, :ctime,
+                            :writable_real?, :executable?, :executable_real?, :sticky?, :owned?,
+                            :grpowned?, :setuid?, :setgid?, :zero?, :size, :size?, :mtime, :atime
+
+        def blocks
+          nil
+        end
+
+        def get_entry
+          @zip_fs_file.__send__(:get_entry, @entry_name)
+        end
+        private :get_entry
+
+        def gid
+          e = get_entry
+          if e.extra.member? 'IUnix'
+            e.extra['IUnix'].gid || 0
+          else
+            0
+          end
+        end
+
+        def uid
+          e = get_entry
+          if e.extra.member? 'IUnix'
+            e.extra['IUnix'].uid || 0
+          else
+            0
+          end
+        end
+
+        def ino
+          0
+        end
+
+        def dev
+          0
+        end
+
+        def rdev
+          0
+        end
+
+        def rdev_major
+          0
+        end
+
+        def rdev_minor
+          0
+        end
+
+        def ftype
+          if file?
+            'file'
+          elsif directory?
+            'directory'
+          else
+            raise StandardError, 'Unknown file type'
+          end
+        end
+
+        def nlink
+          1
+        end
+
+        def blksize
+          nil
+        end
+
+        def mode
+          e = get_entry
+          if e.fstype == 3
+            e.external_file_attributes >> 16
+          else
+            33_206 # 33206 is equivalent to -rw-rw-rw-
+          end
+        end
+      end
+    end
+  end
+end
