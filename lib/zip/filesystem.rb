rubyzip/rubyzip

commit 0e4dc676a0c447537d233b5738da7ecddc26e12e

fix frozen string literal error

lib/zip/filesystem.rb
@@ -239,7 +239,7 @@ module Zip
       end
 
       def open(filename, mode = 'r', permissions = 0o644, &block)
-        mode.delete!('b') # ignore b option
+        mode = mode.tr('b', '') # ignore b option
         case mode
         when 'r'
           @mapped_zip.get_input_stream(filename, &block)
@@ -619,7 +619,7 @@ module Zip
       end
 
       def expand_path(path)
-        expanded = path.start_with?('/') ? path : ::File.join(@pwd, path)
+        expanded = ::File.expand_path(path, @pwd)
         expanded.gsub!(/\/\.(\/|$)/, '')
         expanded.gsub!(/[^\/]+\/\.\.(\/|$)/, '')
         expanded.empty? ? '/' : expanded

lib/zip/extra_field/ntfs.rb
@@ -51,7 +51,7 @@ module Zip
       # reserved 0 and tag 1
       s = [0, 1].pack('Vv')
 
-      tag1 = ''.force_encoding(Encoding::BINARY)
+      tag1 = (+'').force_encoding(Encoding::BINARY)
       if @mtime
         tag1 << [to_ntfs_time(@mtime)].pack('Q<')
         if @atime

lib/zip/extra_field/zip64.rb
@@ -59,7 +59,7 @@ module Zip
 
     def pack_for_c_dir
       # central directory entries contain only fields that didn't fit in the main entry part
-      packed = ''.force_encoding('BINARY')
+      packed = (+'').force_encoding('BINARY')
       packed << [@original_size].pack('Q<') if @original_size
       packed << [@compressed_size].pack('Q<') if @compressed_size
       packed << [@relative_header_offset].pack('Q<') if @relative_header_offset

lib/zip/file.rb
@@ -133,7 +133,7 @@ module Zip
 
       # Same as #open. But outputs data to a buffer instead of a file
       def add_buffer
-        io = ::StringIO.new('')
+        io = ::StringIO.new(+'')
         zf = ::Zip::File.new(io, true, true)
         yield zf
         zf.write_buffer(io)

lib/zip/inflater.rb
@@ -7,7 +7,7 @@ module Zip
       @zlib_inflater = ::Zlib::Inflate.new(-Zlib::MAX_WBITS)
     end
 
-    def read(length = nil, outbuf = '')
+    def read(length = nil, outbuf = +'')
       return (length.nil? || length.zero? ? '' : nil) if eof
 
       while length.nil? || (@buffer.bytesize < length)

lib/zip/ioextras.rb
@@ -6,14 +6,14 @@ module Zip
 
     class << self
       def copy_stream(ostream, istream)
-        ostream.write(istream.read(CHUNK_SIZE, '')) until istream.eof?
+        ostream.write(istream.read(CHUNK_SIZE, +'')) until istream.eof?
       end
 
       def copy_stream_n(ostream, istream, nbytes)
         toread = nbytes
         while toread > 0 && !istream.eof?
           tr = toread > CHUNK_SIZE ? CHUNK_SIZE : toread
-          ostream.write(istream.read(tr, ''))
+          ostream.write(istream.read(tr, +''))
           toread -= tr
         end
       end

lib/zip/ioextras/abstract_input_stream.rb
@@ -11,13 +11,13 @@ module Zip
         super
         @lineno        = 0
         @pos           = 0
-        @output_buffer = ''
+        @output_buffer = +''
       end
 
       attr_accessor :lineno
       attr_reader :pos
 
-      def read(number_of_bytes = nil, buf = '')
+      def read(number_of_bytes = nil, buf = +'')
         tbuf = if @output_buffer.bytesize > 0
                  if number_of_bytes && number_of_bytes <= @output_buffer.bytesize
                    @output_buffer.slice!(0, number_of_bytes)
@@ -93,7 +93,7 @@ module Zip
 
       def flush
         ret_val        = @output_buffer
-        @output_buffer = ''
+        @output_buffer = +''
         ret_val
       end
 

lib/zip/pass_thru_decompressor.rb
@@ -5,7 +5,7 @@ module Zip
       @read_so_far = 0
     end
 
-    def read(length = nil, outbuf = '')
+    def read(length = nil, outbuf = +'')
       return (length.nil? || length.zero? ? '' : nil) if eof
 
       if length.nil? || (@read_so_far + length) > decompressed_size

commit bd6771e684ebe13066efedfbe442f47d096f9702

Fix FileSystem::ZipFileNameMapper#expand_path on Windows
Fixes regression introduced by 0e4dc676a0c447537d233b5738da7ecddc26e12e.

lib/zip/filesystem.rb
@@ -620,7 +620,7 @@ module Zip
       end
 
       def expand_path(path)
-        expanded = ::File.expand_path(path, @pwd)
+        expanded = path.start_with?('/') ? path.dup : ::File.join(@pwd, path)
         expanded.gsub!(/\/\.(\/|$)/, '')
         expanded.gsub!(/[^\/]+\/\.\.(\/|$)/, '')
         expanded.empty? ? '/' : expanded