b2a5f792d476e99ad215735968e805856b43440d
machty <machty@gmail.com>
2023-03-06 15:18:58 -0500

    fix path

--- test/yjit_test.rb
+++ test/yjit_test.rb
@@ -3,7 +3,7 @@
 require 'zip'
 
 driver_name = 'chromedriver'
-Zip::File.open('.chromedriver_linux64.zip') do |zip_file|
+Zip::File.open('./chromedriver_linux64.zip') do |zip_file|
   driver = zip_file.get_entry(driver_name)
   f_path = File.join(Dir.pwd, driver.name)
   FileUtils.mkdir_p(File.dirname(f_path)) unless File.exist?(File.dirname(f_path))
