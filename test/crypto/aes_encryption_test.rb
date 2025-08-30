c21e326fe55e0a7ef21f673aa92436c3e3279f36
Robert Haines <rhaines@manchester.ac.uk>
2025-08-30 16:26:45 +0100

    Implement a couple of sensible copilot review suggestions.

--- test/crypto/aes_encryption_test.rb
+++ test/crypto/aes_encryption_test.rb
@@ -17,20 +17,24 @@ class AESDecrypterTest < MiniTest::Test
   end
 
   def test_decrypt_aes256
-    @decrypter256.reset!([125, 138, 163, 42, 19, 1, 155, 66, 203, 174, 183, 235, 197, 122, 232, 68, 252, 225].pack('C*'))
+    header = [125, 138, 163, 42, 19, 1, 155, 66, 203, 174, 183, 235, 197, 122, 232, 68, 252, 225].pack('C*')
+    @decrypter256.reset!(header)
     assert_equal 'a', @decrypter256.decrypt([161].map(&:chr).join)
   end
 
   def test_decrypt_aes128
-    @decrypter128.reset!([127, 254, 117, 113, 255, 209, 171, 131, 179, 106].pack('C*'))
-    assert_equal [75, 4, 0], @decrypter128.decrypt([34, 33, 106].map(&:chr).join).chars.map(&:ord)
+    header = [127, 254, 117, 113, 255, 209, 171, 131, 179, 106].pack('C*')
+    @decrypter128.reset!(header)
+    assert_equal [75, 4, 0].pack('C*'), @decrypter128.decrypt([34, 33, 106].map(&:chr).join)
   end
 
   def test_reset!
-    @decrypter256.reset!([125, 138, 163, 42, 19, 1, 155, 66, 203, 174, 183, 235, 197, 122, 232, 68, 252, 225].pack('C*'))
+    header = [125, 138, 163, 42, 19, 1, 155, 66, 203, 174, 183, 235, 197, 122, 232, 68, 252, 225].pack('C*')
+    @decrypter256.reset!(header)
     assert_equal 'a', @decrypter256.decrypt([161].map(&:chr).join)
 
-    @decrypter256.reset!([118, 221, 166, 27, 165, 141, 24, 122, 227, 197, 52, 135, 222, 67, 221, 92, 231, 117].pack('C*'))
+    header = [118, 221, 166, 27, 165, 141, 24, 122, 227, 197, 52, 135, 222, 67, 221, 92, 231, 117].pack('C*')
+    @decrypter256.reset!(header)
     assert_equal 'b', @decrypter256.decrypt([135].map(&:chr).join)
   end
 end
