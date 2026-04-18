c67d99f346928ddd6adb802eee70e7063a6f3e2d
Robert Haines <rhaines@manchester.ac.uk>
2026-04-18 16:40:48 +0100

    Document `InputStream` fully.
    This means we need to add documentation to `AbstractInputStream` too.

--- lib/zip/ioextras/abstract_input_stream.rb
+++ lib/zip/ioextras/abstract_input_stream.rb
@@ -5,20 +5,34 @@ module Zip
     # Implements many of the convenience methods of IO
     # such as gets, getc, read, readline and readlines
     # depends on: input_finished?, produce_input and read
-    module AbstractInputStream # :nodoc:
+    module AbstractInputStream
       include Enumerable
       include FakeIO
 
-      def initialize
+      def initialize # :nodoc:
         super
         @lineno        = 0
         @pos           = 0
         @output_buffer = +''.b
       end
 
+      # Returns (or sets) the current line number in the decompressed
+      # (possibly decrypted) data stream. See the Line Number documentation
+      # for the IO class for more information.
       attr_accessor :lineno
+
+      # Returns the current position (in bytes) in the decompressed (possibly
+      # decrypted) data stream.
       attr_reader :pos
 
+      # Reads bytes from the stream decompressed (possibly decrypted) data
+      # stream. If `maxlen` is `nil`, reads all bytes; otherwise, reads up to
+      # `maxlen` bytes. If `maxlen` is zero, returns an empty string.
+      #
+      # Returns a string (either a new string or the given `out_string`)
+      # containing the bytes read. The string's encoding is the unchanged
+      # encoding of `out_string`, if `out_string` is given; `ASCII-8BIT`,
+      # otherwise.
       def read(maxlen = nil, out_string = nil) # rubocop:disable Metrics/PerceivedComplexity
         return (maxlen.nil? || maxlen.zero? ? '' : nil) if eof?
 
@@ -53,10 +67,48 @@ module Zip
         end
       end
 
+      # Reads and returns all remaining lines from the stream. See the Line IO
+      # documentation in the IO class for more information.
+      #
+      # With no arguments given, returns lines as determined by line
+      # separator `$/`, or `nil` if none.
+      #
+      # With only string argument `sep` given, returns lines as
+      # determined by line separator `sep`, or `nil` if none. See the
+      # Line Separator documentation in the IO class for more information.
+      # The two special values for `sep` (`nil` and `""`) are honoured.
+      #
+      # With only integer argument `limit` given, limits the number of bytes
+      # in each line; see the Line Limit documentation in the IO class for more
+      # information.
+      #
+      # With arguments `sep` and `limit` given, combines the two behaviors.
+      #
+      # Optional keyword argument `chomp` specifies whether line separators
+      # are to be omitted.
       def readlines(sep = $INPUT_RECORD_SEPARATOR, limit = nil, chomp: false)
         each(sep, limit, chomp: chomp).to_a
       end
 
+      # Reads and returns a line from the stream. See the Line IO
+      # documentation in the IO class for more information.
+      #
+      # With no arguments given, returns the next line as determined by line
+      # separator `$/`, or `nil` if none.
+      #
+      # With only string argument `sep` given, returns the next line as
+      # determined by line separator `sep`, or `nil` if none. See the
+      # Line Separator documentation in the IO class for more information.
+      # The two special values for `sep` (`nil` and `""`) are honoured.
+      #
+      # With only integer argument `limit` given, limits the number of bytes
+      # in the line; see the Line Limit documentation in the IO class for more
+      # information.
+      #
+      # With arguments `sep` and `limit` given, combines the two behaviors.
+      #
+      # Optional keyword argument `chomp` specifies whether line separators
+      # are to be omitted.
       def gets(sep = $INPUT_RECORD_SEPARATOR, limit = nil, chomp: false)
         if sep.nil?
           return nil if eof?
@@ -95,20 +147,45 @@ module Zip
         chomp ? @output_buffer.slice!(0, cut_index).chomp(sep) : @output_buffer.slice!(0, cut_index)
       end
 
-      def ungetc(byte)
+      def ungetc(byte) # :nodoc:
         @output_buffer = byte.chr + @output_buffer
       end
 
-      def flush
+      def flush # :nodoc:
         @output_buffer.slice!(0..)
       end
 
+      # Reads a line as with #gets, but raises `EOFError` if already at
+      # end-of-stream.
+      #
+      # Optional keyword argument `chomp` specifies whether line separators
+      # are to be omitted.
       def readline(sep = $INPUT_RECORD_SEPARATOR, limit = nil, chomp: false)
         raise EOFError if eof?
 
         gets(sep, limit, chomp: chomp)
       end
 
+      # Calls the block with each remaining line read from the stream.
+      # Does nothing if already at end-of-stream. See the Line IO
+      # documentation in the IO class for more information.
+      #
+      # With no arguments given, reads lines as determined by line separator
+      # `$/`. With only string argument `sep` given, reads lines as determined
+      # by line separator `sep`. See the Line Separator documentation in the
+      # IO class for more information. The two special values for `sep`
+      # (`nil` and `""`) are honoured.
+      #
+      # With only integer argument `limit` given, limits the number of bytes
+      # in each line; see the Line Limit documentation in the IO class for
+      # more information.
+      #
+      # With arguments `sep` and `limit` given, combines the two behaviors.
+      #
+      # Optional keyword argument `chomp` specifies whether line separators
+      # are to be omitted.
+      #
+      # Returns an `Enumerator` if no block is given.
       def each(sep = $INPUT_RECORD_SEPARATOR, limit = nil, chomp: false)
         return to_enum(:each, sep, limit, chomp: chomp) unless block_given?
 
@@ -119,12 +196,15 @@ module Zip
 
       alias each_line each
 
+      # Returns `true` if the stream is positioned at its end, `false`
+      # otherwise. See Position documentation in the IO class for more
+      # information.
       def eof?
         @output_buffer.empty? && input_finished?
       end
 
       # Alias for compatibility. Remove for version 4.
-      alias eof eof?
+      alias eof eof? # :nodoc:
     end
   end
 end
